5b0f11b97aef197f2398208cd4fbef7a339c983a
Michael Santos <100429563+hellomikesantos@users.noreply.github.com>
2023-04-07 21:49:15 -0500

    Initialize Project

---
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import React from 'react';
+import { View, ScrollView, SafeAreaView, Text } from 'react-native';
+
+
+import styles from './nearbyjobcard.style'
+
+const NearbyJobCard = () => {
+  return (
+    <View>
+      <Text>NearbyJobCard</Text>
+    </View>
+  )
+}
+
+export default NearbyJobCard
---
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { TouchableOpacity, Image } from 'react-native';
+
+import styles from './screenheader.style'
+ 
+const ScreenHeaderBtn = () => {
+  return (
+    <TouchableOpacity>
+      <Image 
+        source={iconUrl}
+      />
+    </TouchableOpacity>
+  )
+}
+
+export default ScreenHeaderBtn
